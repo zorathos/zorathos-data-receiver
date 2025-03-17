b2292fd5a60303fa5f3964c7efa2c010c860070e
wangminan <wangminan0811@hotmail.com>
2025-03-17 13:16:25 +0800

    【FIX】包移动与优化import

--- data-receiver-human-machine/src/main/java/org/datacenter/receiver/BaseReceiver.java
+++ data-receiver-human-machine/src/main/java/org/datacenter/receiver/BaseReceiver.java
@@ -1,7 +1,7 @@
 package org.datacenter.receiver;
 
 import lombok.extern.slf4j.Slf4j;
-import org.datacenter.config.receiver.BaseReceiverConfig;
+import org.datacenter.config.BaseReceiverConfig;
 
 /**
  * @author : [wangminan]
