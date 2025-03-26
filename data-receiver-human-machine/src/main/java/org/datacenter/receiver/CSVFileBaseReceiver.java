33e28332d6ada5cbdf0b9abecfddfbbbf2c0870b
Ning Aocheng <1831243781@qq.com>
2025-03-26 17:30:55 +0800

    提取CSV文件接收器基类

---
+++ data-receiver-human-machine/src/main/java/org/datacenter/receiver/CSVFileBaseReceiver.java
@@ -0,0 +1,114 @@
+package org.datacenter.receiver;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.connector.jdbc.JdbcStatementBuilder;
+import org.apache.flink.connector.jdbc.sink.JdbcSink;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.formats.csv.CsvReaderFormat;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.function.SerializableFunction;
+import org.apache.flink.util.function.SerializableSupplier;
+import org.datacenter.config.BaseReceiverConfig;
+import org.datacenter.exception.ZorathosException;
+import org.datacenter.model.base.TiDBDatabase;
+import org.datacenter.model.base.TiDBTable;
+import org.datacenter.receiver.util.DataReceiverUtil;
+import org.datacenter.receiver.util.JdbcSinkUtil;
+import java.io.Serial;
+import java.io.Serializable;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+/**
+ * @author : [ning]
+ * @description : CSV数据接收器基类
+ */
+@Slf4j
+@Data
+@EqualsAndHashCode(callSuper = true)
+public abstract class CSVFileBaseReceiver<T,C extends BaseReceiverConfig> extends BaseReceiver implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1231445L;
+    protected TiDBDatabase database;
+    protected C config;
+    protected TiDBTable table;
+    protected transient Class<T> modelClass;
+    protected String url;
+
+
+    @Override
+    public void prepare() {
+        super.prepare();
+
+
+    }
+
+    // 定义抽象方法：获取CSV列定义
+    protected abstract SerializableFunction<CsvMapper, CsvSchema> getSchemaGenerator();
+
+    // 定义抽象方法：获取插入SQL语句
+    protected abstract String getInsertQuery();
+
+    // 定义抽象方法：绑定PreparedStatement参数
+    protected abstract void bindPreparedStatement(PreparedStatement preparedStatement, T data, String sortieNumber) throws SQLException;
+
+    // 简化版的JdbcStatementBuilder获取方法
+    protected abstract JdbcStatementBuilder<T> getJdbcStatementBuilder();
+
+    // 构建CsvReaderFormat
+    protected CsvReaderFormat<T> buildCsvReaderFormat() {
+        return CsvReaderFormat.forSchema(
+                (SerializableSupplier<CsvMapper>) () -> {
+                    CsvMapper csvMapper = new CsvMapper();
+                    // 注册JavaTimeModule以支持日期时间类型
+                    csvMapper.registerModule(new JavaTimeModule());
+                    return csvMapper;
+                },
+                getSchemaGenerator(),
+                TypeInformation.of(modelClass)
+        );
+    }
+
+    // 构建FileSource
+    protected FileSource<T> buildFileSource(CsvReaderFormat<T> csvReaderFormat) {
+        return FileSource.forRecordStreamFormat(csvReaderFormat, new Path(url)).build();
+    }
+
+    // 构建JdbcSink
+    protected JdbcSink<T> buildJdbcSink() {
+        return JdbcSink.<T>builder()
+                .withQueryStatement(getInsertQuery(), getJdbcStatementBuilder())
+                .withExecutionOptions(JdbcSinkUtil.getTiDBJdbcExecutionOptions())
+                .buildAtLeastOnce(JdbcSinkUtil.getTiDBJdbcConnectionOptions(database));
+    }
+
+    @Override
+    public void start() {
+        StreamExecutionEnvironment env = DataReceiverUtil.prepareStreamEnv();
+        CsvReaderFormat<T> csvReaderFormat = buildCsvReaderFormat();
+
+        FileSource<T> fileSource = buildFileSource(csvReaderFormat);
+        JdbcSink<T> sink = buildJdbcSink();
+
+        env.fromSource(fileSource, WatermarkStrategy.noWatermarks(), modelClass.getName() + " File Source")
+                .returns(modelClass)
+                .sinkTo(sink)
+                .name(modelClass.getName() + " File Sink");
+        try {
+            env.execute(modelClass.getName() + " File Receiver");
+
+        } catch (Exception e) {
+            throw new ZorathosException(e, "Error occurs while executing the Flink job.");
+        }
+    }
+}
--- data-receiver-human-machine/src/main/java/org/datacenter/receiver/simulation/SimulationBaseReceiver.java
+++ data-receiver-human-machine/src/main/java/org/datacenter/receiver/simulation/SimulationBaseReceiver.java
@@ -3,32 +3,17 @@ package org.datacenter.receiver.simulation;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.connector.file.src.FileSource;
 import org.apache.flink.connector.jdbc.JdbcStatementBuilder;
-import org.apache.flink.connector.jdbc.sink.JdbcSink;
-import org.apache.flink.core.fs.Path;
-import org.apache.flink.formats.csv.CsvReaderFormat;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.util.function.SerializableFunction;
-import org.apache.flink.util.function.SerializableSupplier;
 import org.datacenter.config.simulation.SimulationReceiverConfig;
 import org.datacenter.exception.ZorathosException;
 import org.datacenter.model.base.TiDBDatabase;
-import org.datacenter.model.base.TiDBTable;
-import org.datacenter.receiver.BaseReceiver;
-import org.datacenter.receiver.util.DataReceiverUtil;
+import org.datacenter.receiver.CSVFileBaseReceiver;
 import org.datacenter.receiver.util.JdbcSinkUtil;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import static org.datacenter.config.system.BaseSysConfig.humanMachineProperties;
@@ -40,22 +25,19 @@ import static org.datacenter.config.system.BaseSysConfig.humanMachineProperties;
 @Slf4j
 @Data
 @EqualsAndHashCode(callSuper = true)
-public abstract class SimulationBaseReceiver<T> extends BaseReceiver implements Serializable {
+
+public abstract class SimulationBaseReceiver<T> extends CSVFileBaseReceiver<T, SimulationReceiverConfig> implements Serializable {
     @Serial
-    private static final long serialVersionUID = 1231445L;
-
-    protected SimulationReceiverConfig config;
-    protected TiDBTable table;
-    protected transient Class<T> modelClass;
-
+    private static final long serialVersionUID = 2345678L;
 
     @Override
     public void prepare() {
         super.prepare();
-
+        database = TiDBDatabase.SIMULATION;
+        url = config.getUrl();
         // 通过JDBC连接到对应j表 如果有和config.getSortieNumber()相同的记录就删除
         try {
-            log.info("Linking to table: {}.{} for preparation.", TiDBDatabase.SIMULATION.getName(), table.getName());
+            log.info("Linking to table: {}.{} for preparation.", database.getName(), table.getName());
             Class.forName(humanMachineProperties.getProperty("tidb.driverName"));
             Connection connection = DriverManager.getConnection(
                     JdbcSinkUtil.TIDB_URL_SIMULATION,
@@ -77,7 +59,7 @@ public abstract class SimulationBaseReceiver<T> extends BaseReceiver implements 
                     var deletePreparedStatement = connection.prepareStatement(deleteSql);
                     deletePreparedStatement.setString(1, config.getSortieNumber());
                     deletePreparedStatement.executeUpdate();
-                    log.info("Delete {} records from table: {}.{}.", count, TiDBDatabase.SIMULATION.getName(), table.getName());
+                    log.info("Delete {} records from table: {}.{}.", count, database.getName(), table.getName());
                 } else {
                     // 如果没有对应记录 新建分区
                     String createPartitionSql = """
@@ -85,7 +67,7 @@ public abstract class SimulationBaseReceiver<T> extends BaseReceiver implements 
                             """.formatted(table.getName());
                     var createPreparedStatement = connection.prepareStatement(createPartitionSql);
                     createPreparedStatement.executeUpdate();
-                    log.info("Create partition for table: {}.{}.", TiDBDatabase.SIMULATION.getName(), table.getName());
+                    log.info("Create partition for table: {}.{}.", database.getName(), table.getName());
                 }
             }
             connection.close();
@@ -95,67 +77,10 @@ public abstract class SimulationBaseReceiver<T> extends BaseReceiver implements 
         }
     }
 
-    // 定义抽象方法：获取CSV列定义
-    protected abstract SerializableFunction<CsvMapper, CsvSchema> getSchemaGenerator();
-
-    // 定义抽象方法：获取插入SQL语句
-    protected abstract String getInsertQuery();
-
-    // 定义抽象方法：绑定PreparedStatement参数
-    protected abstract void bindPreparedStatement(PreparedStatement preparedStatement, T data, String sortieNumber) throws SQLException;
-
-    // 简化版的JdbcStatementBuilder获取方法
+    @Override
     protected JdbcStatementBuilder<T> getJdbcStatementBuilder() {
-        // 保存为final变量以避免捕获外部实例
         final String sortieNumber = config.getSortieNumber();
 
         return (statement, data) -> bindPreparedStatement(statement, data, sortieNumber);
-    }
-
-    // 构建CsvReaderFormat
-    protected CsvReaderFormat<T> buildCsvReaderFormat() {
-        return CsvReaderFormat.forSchema(
-                (SerializableSupplier<CsvMapper>) () -> {
-                    CsvMapper csvMapper = new CsvMapper();
-                    // 注册JavaTimeModule以支持日期时间类型
-                    csvMapper.registerModule(new JavaTimeModule());
-                    return csvMapper;
-                },
-                getSchemaGenerator(),
-                TypeInformation.of(modelClass)
-        );
-    }
-
-    // 构建FileSource
-    protected FileSource<T> buildFileSource(CsvReaderFormat<T> csvReaderFormat) {
-        return FileSource.forRecordStreamFormat(csvReaderFormat, new Path(config.getUrl())).build();
-    }
-
-    // 构建JdbcSink
-    protected JdbcSink<T> buildJdbcSink() {
-        return JdbcSink.<T>builder()
-                .withQueryStatement(getInsertQuery(), getJdbcStatementBuilder())
-                .withExecutionOptions(JdbcSinkUtil.getTiDBJdbcExecutionOptions())
-                .buildAtLeastOnce(JdbcSinkUtil.getTiDBJdbcConnectionOptions(TiDBDatabase.SIMULATION));
-    }
-
-    @Override
-    public void start() {
-        StreamExecutionEnvironment env = DataReceiverUtil.prepareStreamEnv();
-        CsvReaderFormat<T> csvReaderFormat = buildCsvReaderFormat();
-
-        FileSource<T> fileSource = buildFileSource(csvReaderFormat);
-        JdbcSink<T> sink = buildJdbcSink();
-
-        env.fromSource(fileSource, WatermarkStrategy.noWatermarks(), modelClass.getName() + " File Source")
-                .returns(modelClass)
-                .sinkTo(sink)
-                .name(modelClass.getName() + " File Sink");
-        try {
-            env.execute(modelClass.getName() + " File Receiver");
-
-        } catch (Exception e) {
-            throw new ZorathosException(e, "Error occurs while executing the Flink job.");
-        }
     }
 }
